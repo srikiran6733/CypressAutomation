d716ca92226025a2191dedf18362c464270ff37d
srikiran6733 <kirankumarchennuboina@gmail.com>
2025-11-02 21:57:38 +0530

    modified the cypress config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,17 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  // in cypress config outside of env and inside of e2e
-reporter: "mocha-junit-reporter",
-reporterOptions: {
-  mochaFile: "results/test-results-[hash].xml",
-  includePending: true,
-  testCaseSwitchClassnameAndName: true,
-},
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+  reporter: "mocha-junit-reporter",
+  reporterOptions: {
+    mochaFile: "results/test-results-[hash].xml",
+    includePending: true,
+    testCaseSwitchClassnameAndName: true
   },
 
   e2e: {
+    specPattern: "cypress/tests/**/*.cy.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-    specPattern: "cypress/tests/**/*.cy.js"
-  },
+    }
+  }
 });
